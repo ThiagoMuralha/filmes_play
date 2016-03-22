8d4c1a9af9cbd3763642fc39975f8fb065698123
Thiago Medeiros <si-thiago@hotmail.com>
2016-03-22 02:38:13 -0300

    CRUD diretor terminado

--- app/controllers/DiretorCRUD.java
+++ app/controllers/DiretorCRUD.java
@@ -1,24 +1,80 @@
 package controllers;
 
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.diretor;
 import models.Diretor;
 import java.util.List;
+import com.avaje.ebean.Ebean;
+import javax.inject.Inject;
 
 public class DiretorCRUD extends Controller {
-	List<Diretor> diretores;
 	
+	@Inject
+	FormFactory formFactory;
 	
 	public Result lista() {
-		diretores = Diretor.find.findList();
+		List<Diretor> diretores = Diretor.find.findList();
 		for(Diretor diretor : diretores) {
-			System.out.println(diretor.id + " " + diretor.nome);
+			System.out.println(diretor.getId() + " " + diretor.getNome());
 		}
-		return ok(views.html.diretor.render()); 
+		return ok(views.html.diretor.render(diretores)); 
 	}
 	
+	
+	
+	public Result detalhar(Long id) {
+		Form<Diretor> dirForm = formFactory.form(Diretor.class).fill(Diretor.find.byId(id));
+		return ok(views.html.alterarDiretor.render(id, dirForm));
+	}
+	
+	
+	
+	public Result alterar(Long id) {
+		Form<Diretor> atualForm = formFactory.form(Diretor.class).fill(Diretor.find.byId(id));		
+		Form<Diretor> alterarForm = formFactory.form(Diretor.class).bindFromRequest();
+		if(alterarForm.hasErrors()) {
+			return badRequest(views.html.alterarDiretor.render(id, alterarForm));
+		}
+		atualForm.get().setNome(alterarForm.get().getNome());
+		atualForm.get().update();
+		flash("sucesso", "Diretor " + atualForm.get().getNome() + " e id " + atualForm.get().getId() + " alterado com sucesso!");
+		return redirect(routes.DiretorCRUD.lista());
+	}
+	
+	
+	
+	public Result remover(Long id) {
+		String nomeDiretor = Diretor.find.byId(id).getNome();
+		Diretor.find.ref(id).delete();
+		flash("sucesso", "Diretor " + nomeDiretor + " removido com sucesso");
+		return lista();
+	}
+	
+	
+	
 	public Result novoDiretor() {
-		return TODO;
+		if(formFactory.form(Diretor.class) != null)
+			System.out.println("O fera não ta nulo");
+		else
+			System.out.println("Tá nulo sim");
+		return ok(views.html.novoDiretor.render(formFactory.form(Diretor.class)));
+	}
+	
+	
+	
+	public Result gravar() {
+		Form<Diretor> form = formFactory.form(Diretor.class).bindFromRequest();
+		
+		if (form.bindFromRequest().hasErrors()) {
+			flash("erro","Foram identificados problemas no cadastro!");
+			return ok(views.html.novoDiretor.render(formFactory.form(Diretor.class)));
+		}
+		else {
+			form.get().save();
+			flash("sucesso", "Registro gravado com sucesso!");
+			return redirect(routes.DiretorCRUD.lista());
+		}
 	}
 }
--- app/models/Diretor.java
+++ app/models/Diretor.java
@@ -9,11 +9,28 @@ public class Diretor extends Model {
 	
 	private static final long serialVersionUID = 1l;
 
+	public static Finder<Long, Diretor> find = new Finder<Long, Diretor>(Diretor.class);
+
 	@Id
-	public Long id;
-
+	private Long id;
 	@Constraints.Required
-	public String nome;
+	private String nome;
+	
+	// getters e setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-	public static Model.Finder<Long, Diretor> find = new Model.Finder<Long, Diretor>(Long.class, Diretor.class);
+	public String toString() {
+		return "nome = " + this.nome + " e ID = " + this.id;
+	}
 }
